74aa8d17560243ddb2ffea2b0e6c7d8c97a70133
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-02-17 21:49:16 +0100

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,9 @@
 function toggle_text() {
-    var span = document.getElementById("span_txt");
+    const span = document.getElementById("span_txt");
+
     if(span.style.display == "none") {
-      span.style.display = "inline";
+        span.style.display = "inline";
     } else {
-      span.style.display = "none";
+        span.style.display = "none";
     }
-  }
+};
