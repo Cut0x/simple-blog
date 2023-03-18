98f4aa5186d22c7991c1874e904eebd889a78e78
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-03-18 11:48:13 +0100

    Update index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    //
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- SITE VEAWER -->
+    <title></title>
+    <link rel="shortcut icon" href="" type="image/x-icon">
+    <meta name="description" content="">
+
+    <!-- STYLE AND FRAMEWORK -->
+    <link rel="stylesheet" href="./src/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+</head>
+<body>
+    
+</body>
+</html>
